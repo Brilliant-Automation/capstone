90542c4222c6c5f3a82a01483bb9173b6233cee1
mdah <mda.hewlett@gmail.com>
2025-06-08 11:49:18 -0700

    refactor: major cleanup of styles

--- dashboard/src/components/layout.py
+++ dashboard/src/components/layout.py
@@ -3,38 +3,40 @@ import dash_bootstrap_components as dbc
 from components.dropdowns import create_date_range_dropdown, device_dropdown, dual_sensor_dropdown
 from components.radar_chart import radar_chart_1, radar_chart_2
 from components.overlay_plot import rating_health_chart, frequency_chart
-from components.signal_charts import signal_charts_column
+from components.signal_charts import signal_chart_sig_raw, signal_chart_env, signal_chart_sig_fft, signal_chart_env_fft
 from components.header_timestamp import header_timestamp
 
 def summary_view():
-    return html.Div([
+    return[
         # Rating radar charts
         dbc.Row([
             dbc.Col(radar_chart_1),
             dbc.Col(radar_chart_2)
-        ], className="radar-row"),
+        ], className="graph-row"),
 
         # Rating overlay
         dbc.Row([
-            dbc.Col(rating_health_chart)
-        ], className="frequency-row")
-    ], className="dashboard-col")
+            dbc.Col(rating_health_chart, width=12)
+        ], className="graph-row")
+    ]
 
 def signal_view():
-    return html.Div([
+    return [
         # Signal overlay
         dbc.Row([
             dbc.Col(frequency_chart)
-        ], className="frequency-row"),
+        ], className="graph-row"),
 
         # Signal charts
         dbc.Row([
-            dcc.Graph(id="signal-chart-sig-raw", className="signal-chart", config={"displayModeBar": False}),
-            dcc.Graph(id="signal-chart-sig-fft", className="signal-chart", config={"displayModeBar": False}),
-            dcc.Graph(id="signal-chart-env", className="signal-chart", config={"displayModeBar": False}),
-            dcc.Graph(id="signal-chart-env-fft", className="signal-chart", config={"displayModeBar": False}),
-        ], className="signal-row")
-    ], style={"height": "100%", "width": "100%"})
+            dbc.Col(signal_chart_sig_raw),
+            dbc.Col(signal_chart_env),
+        ], className="graph-row"),
+        dbc.Row([
+            dbc.Col(signal_chart_sig_fft),
+            dbc.Col(signal_chart_env_fft),
+        ], className="graph-row")
+    ]
 
 
 def create_layout():
@@ -45,7 +47,7 @@ def create_layout():
             dbc.Col([header_timestamp()]),
         ]),
 
-        # Dropdowns
+        # Device and Time Dropdowns
         dbc.Row([
             dbc.Col(device_dropdown, width=4, className="dropdown-input"),
             dbc.Col(create_date_range_dropdown("start"), width=4),
@@ -66,15 +68,16 @@ def create_layout():
             ])
         ]),
 
-        # Dropdowns
+        # Rating/Location Dropdowns
         dbc.Row([
             dbc.Col(dual_sensor_dropdown, width=12, className="dropdown-input"),
         ]),
 
+        # Graphs
         dbc.Row([
             dbc.Col([
-                html.Div(id="summary-view", children=summary_view(), className="view-container"),
-                html.Div(id="signal-view", children=signal_view(), className="view-container", style={"display": "none"})
-            ], className="dashboard-col")
-        ], className="dashboard-row")
+                html.Div(id="summary-view", children=summary_view(), className="tab-content-container"),
+                html.Div(id="signal-view", children=signal_view(), className="tab-content-container", style={"display": "none"})
+            ])
+        ])
     ], className="dash-container", fluid=True)
