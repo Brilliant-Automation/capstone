d834d1ce8c820f83eb74db4d3913ede4a297b757
mdah <mda.hewlett@gmail.com>
2025-06-08 11:58:51 -0700

    docs: cleanup comments

--- dashboard/src/components/signal_charts.py
+++ dashboard/src/components/signal_charts.py
@@ -6,7 +6,6 @@ from utils.config import FEATURES
 STANDARD_NUMBER_OF_PLOTS = 4
 
 def create_signal_chart(chart_id):
-    """Create a signal chart component with the given ID"""
     return dcc.Graph(
         id=chart_id,
         figure=go.Figure(),
@@ -20,7 +19,6 @@ signal_chart_env = create_signal_chart("signal-chart-env")
 signal_chart_env_fft = create_signal_chart("signal-chart-env-fft")
 
 def update_signal_charts(df):
-    # techdebt: config file where colnames can be put, currently fragile
     if df.empty or FEATURES['vibration_velocity_z'] not in df.columns:
         return [go.Figure()] * STANDARD_NUMBER_OF_PLOTS
     return SignalVisualizer(df).generate()
